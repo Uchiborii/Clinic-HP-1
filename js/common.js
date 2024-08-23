641b582bb038a36c13c126895193ca9a697e0930
Uchiborii <zuumo219@gmail.com>
2024-08-23 16:31:27 +0900

    fix

--- js/common.js
+++ js/common.js
@@ -137,49 +137,7 @@ catchEndUrl().then((params) => {
   displayData();
 });
 
-function toggleNav() {
-  var body = document.body;
-  body.classList.toggle("nav-open");
-}
-var hamburger = document.getElementById("js-hamburger");
-var blackBg = document.getElementById("js-black-bg");
 
-async function getChooseArticle() {
-  const paramsData = await catchEndUrl();
-  console.log("paramsデータ" + paramsData);
-  const value = paramsData.get("id");
-  const url = `${baseUrl}/${value}`;
-  const data = await fetchData(url);
-  displayPost(data);
-}
-
-async function displayPost(data) {
-  getTitle(data);
-  const date = new Date(data.dateTime);
-  const formattedDate = customFormat(date);
-
-  let html = "";
-  const content = `<div class="container">
-	<div class="news-title">
-	<h3>${data.title}</h3>
-	</div>
-	<div>
-	<div class="h5-font">${formattedDate}</div>
-	</div>
-	<div>
-	<p>${data.content}</p>
-	</div>
-	</div>`;
-  html += content;
-  document.getElementById("result").innerHTML = html;
-}
-
-async function getTitle(data) {
-  let html = "";
-  const contents = `<div class="a-font">${data.title}</div>`;
-  html += contents;
-  document.getElementById("title").innerHTML = html;
-}
 
 //採用情報
 
--- js/html.js
+++ js/html.js
@@ -7,3 +7,47 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((response) => response.text())
     .then((data) => (document.querySelector("#footer").innerHTML = data));
 });
+
+function toggleNav() {
+  var body = document.body;
+  body.classList.toggle("nav-open");
+}
+var hamburger = document.getElementById("js-hamburger");
+var blackBg = document.getElementById("js-black-bg");
+
+async function getChooseArticle() {
+  const paramsData = await catchEndUrl();
+  console.log("paramsデータ" + paramsData);
+  const value = paramsData.get("id");
+  const url = `${baseUrl}/${value}`;
+  const data = await fetchData(url);
+  displayPost(data);
+}
+
+async function displayPost(data) {
+  getTitle(data);
+  const date = new Date(data.dateTime);
+  const formattedDate = customFormat(date);
+
+  let html = "";
+  const content = `<div class="container">
+	<div class="news-title">
+	<h3>${data.title}</h3>
+	</div>
+	<div>
+	<div class="h5-font">${formattedDate}</div>
+	</div>
+	<div>
+	<p>${data.content}</p>
+	</div>
+	</div>`;
+  html += content;
+  document.getElementById("result").innerHTML = html;
+}
+
+async function getTitle(data) {
+  let html = "";
+  const contents = `<div class="a-font">${data.title}</div>`;
+  html += contents;
+  document.getElementById("title").innerHTML = html;
+}
